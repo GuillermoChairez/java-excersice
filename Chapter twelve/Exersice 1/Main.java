4bae896f18d9ead8a4eb9eb2366b19f0fb10e7f6
GuillermoChairez <102264935+GuillermoChairez@users.noreply.github.com>
2022-04-07 12:53:28 -0500

    Add files via upload

---
+++ Chapter twelve/Exersice 1/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import com.company.Chapter11.Exersice10.MyStack;
+import com.company.Chapter12.Exersice1.Calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*Scanner input = new Scanner(System.in);
+        System.out.println("Enter five Strings: ");*/
+        String[] s = {"4","+","5"};
+        Calculator.main(s);
+
+    }
+}
